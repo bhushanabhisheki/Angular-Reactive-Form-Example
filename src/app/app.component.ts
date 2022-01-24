7d2ab0d97c29f1018a1860525fe07de0c6978b6a
bhushanabhisheki <bhushanabhisheki@gmail.com>
2022-01-24 12:41:12 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      projectStatus: new FormControl('critical'),
+    });
+  }
+
+  onSaveProject() {
+    console.log(this.projectForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
